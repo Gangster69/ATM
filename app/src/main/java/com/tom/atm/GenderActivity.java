6936ec06d9ee077f3e497158d9d4ebc33d3c2ead
Gangster69 <awed3310@gmail.com>
2018-11-19 21:04:10 +0800

    2018/11/19

--- app/src/main/java/com/tom/atm/GenderActivity.java
+++ app/src/main/java/com/tom/atm/GenderActivity.java
@@ -1,7 +1,10 @@
 package com.tom.atm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class GenderActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class GenderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gender);
+    }
+    public void next(View view){
+        String gender =((EditText) findViewById(R.id.ed_gender)).getText().toString();
+        getSharedPreferences("atm" ,MODE_PRIVATE)
+                .edit()
+                .putString("Gender" , gender)
+                .apply();
+        Intent main =new Intent(this ,MainActivity.class);
+        startActivity(main);
     }
 }
