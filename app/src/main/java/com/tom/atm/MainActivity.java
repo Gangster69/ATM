20371a3ef0c303bbb535fc989379412b1c38eaff
Gangster69 <awed3310@gmail.com>
2018-12-04 03:05:28 +0800

    listView&RecyclerView

--- app/src/main/java/com/tom/atm/MainActivity.java
+++ app/src/main/java/com/tom/atm/MainActivity.java
@@ -1,10 +1,17 @@
 package com.tom.atm;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,6 +20,7 @@ import java.util.function.ToDoubleBiFunction;
 public class MainActivity extends BaseActivity{
     public static final int RC_LOGIN =100;
     boolean logon = false;
+    List<String> fruits = Arrays.asList("香蕉", "鳯梨", "芭樂");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +31,44 @@ public class MainActivity extends BaseActivity{
         }
 
         //ListView
-
-        private void listView () {
-            List<String> fruits = Arrays.asList("香蕉", "鳳梨", "芭樂");
-            ArrayAdapter<String> adapter =
-                    new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, fruits);
-            ListView listView = findViewById(R.id.list);
-            listView.setAdapter(adapter);
+     //      listView();
+        //RecyclerView
+        RecyclerView recyclerView =findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new FruitAdapter());
+    }
+    class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.FruitViewHolder>{
+        @NonNull
+        @Override
+        public FruitViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new FruitViewHolder(view);
         }
+        @Override
+        public void onBindViewHolder(@NonNull FruitViewHolder holder, int position) {
+            holder.nameText.setText(fruits.get(position));
+        }
+        @Override
+        public int getItemCount() {
+            return fruits.size();
+        }
+        class FruitViewHolder extends RecyclerView.ViewHolder {
+            TextView nameText;
+            public FruitViewHolder(View itemView) {
+                super(itemView);
+                nameText = itemView.findViewById(android.R.id.text1);
+            }
+        }
+    }
+
+    private void listView() {
+        List<String> fruits = Arrays.asList("香蕉", "鳳梨", "芭樂");
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter< >(this, android.R.layout.simple_list_item_1, fruits);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 
 
